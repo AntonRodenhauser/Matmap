e669392586d88b7017c6c958047ad678085dadb8
Anton Rodenhauser <anton.rodenhauser@hotmail.com>
2017-07-28 16:46:30 -0600

    ..

---
+++ blaa.m
@@ -0,0 +1,19 @@
+function bla
+
+s1=tic;
+s2=tic;
+a=rand(200,200);
+b=rand(200,200);
+a=rand(200,200);
+b=rand(200,200);
+a=rand(200,200);
+b=rand(200,200);
+toc(s2)
+
+s3=tic;
+
+c=a\b;
+
+toc(s3)
+toc(s1)
+
