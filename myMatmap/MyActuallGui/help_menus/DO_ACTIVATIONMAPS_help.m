89e11afa28c14990330effecfe46fc8209d18267
Anton Rodenhauser <anton.rodenhauser@hotmail.com>
2017-07-28 11:35:30 -0600

    removed myActuall Gui

--- myMatmap/MyActuallGui/help_menus/DO_ACTIVATIONMAPS_help.m
+++
@@ -1,5 +0,0 @@
-%% Create Activation/Recovery maps
-% If this option is ON, Activaton/Recovery maps are saved for each data
-% file. Note that there are no Activations/Recoveries without a QRS wave
-% and a T wave! If  there is no QRS/T wave, the A/R maps are still created
-% and stored, but only contain zeroes!
